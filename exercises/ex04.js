0465a2798b7888525eafe7928554b79a24825ab0
jakemis04 <jakemisraje@gmail.com>
2025-10-14 19:21:58 -0700

    new website

---
+++ exercises/ex04.js
@@ -0,0 +1,27 @@
+
+let totalnumber = 0;
+
+
+
+//add a button titled click me
+
+$("#needy-button").click(function (){}
+
+
+totalnumber= totalnumber + 1;
+
+$("needy-button").html(totalnumber);
+
+})
+
+
+//when it is clicked
+//add one to the total number
+
+//show how many times
+//on our button
+//show "clicked total number of times"
+
+
+//a top limit
+
