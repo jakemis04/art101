c7f984aa5480bf2b43a2d0aa3867842f4ffb9d21
jakemis04 <jakemisraje@gmail.com>
2025-10-28 16:07:27 -0700

    lab5

--- environment/env03.js
+++ environment/env03.js
@@ -4,3 +4,17 @@ let megaSentence;
 megaSentence = "<p>"+ moveSet[0] +", "+ moveSet[1] +", "+ moveSet[2] +", "+ moveSet[3] +"</p>";
 $("#output").html(megaSentence);
 $("#output2").html(megaSentence);
+
+function setBackground(imageFile, labelText) {
+    $("body").css("background-image", `url('${imageFile}')`);
+    $("#output3").html(labelText);
+}
+
+$("#bg1-button").click(function () {
+    setBackground("pokemon_background_night.jpeg", "Seems like it's getting dark!");
+});
+
+
+$("#bg2-button").click(function () {
+    setBackground("pokemon_background.jpeg", "Looks like a sunny morning!");
+});
