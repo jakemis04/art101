dd534423bf1a9c7244c0940f0d1fbfa812f5b1de
jakemis04 <jakemisraje@gmail.com>
2025-11-18 20:52:21 -0800

    lab 8

--- environment/env04.js
+++ environment/env04.js
@@ -1,8 +1,34 @@
+
+let $popup = $("<div class='popup-tip'></div>").appendTo("body").hide();
+
+$(".nice-block span")
+    .mouseenter(function (e) {
+        const text = $(this).parent().data("popup"); 
+        $popup.text(text);
+
+        $popup.css({
+            top: e.pageY + 10,
+            left: e.pageX + 10
+        }).fadeIn(150);
+    })
+    .mousemove(function (e) {
+        
+        $popup.css({
+            top: e.pageY + 10,
+            left: e.pageX + 10
+        });
+    })
+    .mouseleave(function () {
+        $popup.fadeOut(150);
+    });
+
+
+
 $(".nice-block span").click(function () {
 
   const $box = $(this).parent();
   const $text = $(this);
-  const fullHeight = $box.get(0).scrollHeight;
+  const fullHeight=$box.get(0).scrollHeight;
 
   if ($text.html() === "more") {
   
